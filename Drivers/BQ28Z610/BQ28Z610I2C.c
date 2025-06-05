f377382d77b1321b9e248d0eb6272aee5198d881
Joel <joelkubinsky@ufl.edu>
2025-06-05 11:00:04 -0400

    BQZ driver updates. - Joel

--- Drivers/BQ28Z610/BQ28Z610I2C.c
+++ Drivers/BQ28Z610/BQ28Z610I2C.c
@@ -10,7 +10,7 @@
 #include "stm32g4xx_hal.h"
 
 #define CMD_VOLTAGE 0x08
-#define BQ28Z610_I2C_ADDR (0x55 << 1)
+#define BQ28Z610_I2C_ADDR 0x55
 
 HAL_StatusTypeDef BQ28Z610_ReadVoltage(I2C_HandleTypeDef *hi2c, uint16_t *voltage) {
 	uint8_t rx[2];
